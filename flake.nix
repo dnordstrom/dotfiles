01ec1e1a420c24ed11caee8ffeecc76ef1e62ee2
Daniel Nordstrom <d@mrnordstrom.com>
2021-07-22 21:12:57 +0200

    Trying stuff.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+
+  outputs = { self, nixpkgs }: {
+
+    nixosConfigurations.nordix = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+	  "/etc/nixos/configuration.nix"
+	  ({ pkgs, ... }: {
+	    packages = with pkgs; [
+	       chromium
+	    ];
+            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          })
+        ];
+    };
+
+  };
+}
