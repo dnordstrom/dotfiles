a6ed95128bf4a04c1148d2c85a6e59c9f5bfa383
Daniel Nordstrom <d@mrnordstrom.com>
2022-01-29 08:39:05 +0100

    Moving color scheme to correct subdirectory.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "/etc/nixos/config/wezterm/Catppuccin.itermcolors",
+	color_scheme = "/etc/nixos/config/wezterm/colors/catppuccin.itermcolors",
 	font = wezterm.font("PragmataPro Mono Liga", { weight = "Regular", stretch = "Normal", italic = false }),
 	hide_tab_bar_if_only_one_tab = true,
 }
